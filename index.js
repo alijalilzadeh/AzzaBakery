6dbf4344d135bbee1ab5e6f26719f8bdc00e8661
alijalilzadeh <ali.jalilzadeh2007@gmail.com>
2025-11-15 23:33:50 +0400

    Side bar added

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Sidebar Opening/Closing
+
 const hamburgerIcon = document.querySelector('.fa-bars');
 const xMark = document.querySelector(".upper-part .fa-xmark")
 const hamburgerSidebar = document.querySelector(".hamburgerSidebar")
@@ -9,4 +11,25 @@ hamburgerIcon.addEventListener("click",()=>{
 xMark.addEventListener("click",()=>{
     hamburgerSidebar.classList.remove("opening");
     hamburgerSidebar.classList.toggle("closing");
+})
+
+// Navbar Position Fixed
+
+const navbarBox = document.querySelector(".interier-view .navbar-box");
+const azzaSvgLink = document.querySelector(".interier-view .navbar-left a");
+const languageSelection = document.querySelector(".navbar-box .language-selection span");
+
+window.addEventListener("scroll",()=>{
+  if(window.scrollY > 400){
+    navbarBox.classList.add("fixed");
+    languageSelection.classList.add("scroll")
+    hamburgerIcon.style.color = "#4B2613"
+    azzaSvgLink.style.display = "block"
+  }
+  else{
+    languageSelection.classList.remove("scroll")
+    navbarBox.classList.remove("fixed");
+    hamburgerIcon.style.color = "";
+    azzaSvgLink.style.display = "none"
+  }
 })
