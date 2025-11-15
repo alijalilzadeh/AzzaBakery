c518f7a89fc539706bc210fdc5127f8ab7ccbbab
alijalilzadeh <ali.jalilzadeh2007@gmail.com>
2025-11-16 01:27:01 +0400

    Uptaded

--- index.js
+++ index.js
@@ -21,31 +21,34 @@ const azzaSvgLink = document.querySelector(".interier-view .navbar-left a");
 const languageSelectionSpanLang1 = document.querySelector(".navbar-box .language-selection .lang1");
 const languageSelectionOtherLangs = document.querySelectorAll(".navbar-box .language-selection .optional-lang");
 
+// window Scrolling
+
+const phoneNumber = document.querySelector(".navbar-right .phone-number ");
+const phoneNumberText = document.querySelector(".navbar-right .phone-number span");
+const phoneIcon = document.querySelector(".navbar-right .phone-number i");
+
+const contactIcons = document.querySelector(".navbar-right .contact-icons a");
 
 window.addEventListener("scroll", () => {
-  if (window.scrollY > 400) {
-    navbarBox.classList.add("fixed");
-    azzaSvgLink.style.display = "block";
+  const isBigger = window.scrollY > 400;
 
-    languageSelectionSpanLang1.classList.add("scroll");
-    // languageSelectionOtherLangs.forEach(lang => {
-    //   lang.classList.remove("deactive")
-    //   lang.classList.add("active")
-    // });
-    hamburgerIcon.style.color = "#4B2613";
-  } else {
-    navbarBox.classList.remove("fixed");
-    azzaSvgLink.style.display = "none";
-    hamburgerIcon.style.color = "";
+  navbarBox.classList.toggle("fixed", isBigger);
+  azzaSvgLink.style.display = isBigger ? "block" : "none";
 
-    languageSelectionSpanLang1.classList.remove("scroll");
-    //  languageSelectionOtherLangs.forEach(lang => {
-    //   lang.classList.remove("active");
-    //   lang.classList.add("deactive");
+  languageSelectionSpanLang1.classList.toggle("scroll", isBigger);
 
-    // });
-  }
+  hamburgerIcon.style.color = isBigger ? "#4B2613" : "";
+
+  phoneNumber.classList.toggle("bigger400", isBigger);
+  phoneNumberText.classList.toggle("bigger400", isBigger);
+  phoneIcon.classList.toggle("bigger400", isBigger);
+
+  contactIcons.forEach(icon => {
+    icon.classList.toggle("bigger400",isBigger);
+  });
+
 });
+
 
 
 languageSelectionSpanLang1.addEventListener("click", () => {
@@ -53,7 +56,8 @@ languageSelectionSpanLang1.addEventListener("click", () => {
     lang.classList.toggle("active");
   });
 
-  if(window.scrollY > 400){
+  if (window.scrollY > 400) {
+
     languageSelectionOtherLangs.forEach(lang => {
       lang.classList.remove("white");
       lang.classList.add("brown");
