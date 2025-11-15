f6e8add2b16d8b88850120d487956ae5220f0197
alijalilzadeh <ali.jalilzadeh2007@gmail.com>
2025-11-16 01:06:24 +0400

    Language selection upgrated

--- index.js
+++ index.js
@@ -3,33 +3,65 @@
 const hamburgerIcon = document.querySelector('.fa-bars');
 const xMark = document.querySelector(".upper-part .fa-xmark")
 const hamburgerSidebar = document.querySelector(".hamburgerSidebar")
-hamburgerIcon.addEventListener("click",()=>{
+hamburgerIcon.addEventListener("click", () => {
   hamburgerSidebar.classList.remove("closing")
-    hamburgerSidebar.classList.toggle("opening");
+  hamburgerSidebar.classList.toggle("opening");
 })
 
-xMark.addEventListener("click",()=>{
-    hamburgerSidebar.classList.remove("opening");
-    hamburgerSidebar.classList.toggle("closing");
+xMark.addEventListener("click", () => {
+  hamburgerSidebar.classList.remove("opening");
+  hamburgerSidebar.classList.toggle("closing");
 })
 
 // Navbar Position Fixed
 
-const navbarBox = document.querySelector(".interier-view .navbar-box");
+const navbarBox = document.querySelector(".navbar-box");
 const azzaSvgLink = document.querySelector(".interier-view .navbar-left a");
-const languageSelection = document.querySelector(".navbar-box .language-selection span");
 
-window.addEventListener("scroll",()=>{
-  if(window.scrollY > 400){
+const languageSelectionSpanLang1 = document.querySelector(".navbar-box .language-selection .lang1");
+const languageSelectionOtherLangs = document.querySelectorAll(".navbar-box .language-selection .optional-lang");
+
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 400) {
     navbarBox.classList.add("fixed");
-    languageSelection.classList.add("scroll")
-    hamburgerIcon.style.color = "#4B2613"
-    azzaSvgLink.style.display = "block"
-  }
-  else{
-    languageSelection.classList.remove("scroll")
+    azzaSvgLink.style.display = "block";
+
+    languageSelectionSpanLang1.classList.add("scroll");
+    // languageSelectionOtherLangs.forEach(lang => {
+    //   lang.classList.remove("deactive")
+    //   lang.classList.add("active")
+    // });
+    hamburgerIcon.style.color = "#4B2613";
+  } else {
     navbarBox.classList.remove("fixed");
+    azzaSvgLink.style.display = "none";
     hamburgerIcon.style.color = "";
-    azzaSvgLink.style.display = "none"
+
+    languageSelectionSpanLang1.classList.remove("scroll");
+    //  languageSelectionOtherLangs.forEach(lang => {
+    //   lang.classList.remove("active");
+    //   lang.classList.add("deactive");
+
+    // });
   }
-})
+});
+
+
+languageSelectionSpanLang1.addEventListener("click", () => {
+  languageSelectionOtherLangs.forEach(lang => {
+    lang.classList.toggle("active");
+  });
+
+  if(window.scrollY > 400){
+    languageSelectionOtherLangs.forEach(lang => {
+      lang.classList.remove("white");
+      lang.classList.add("brown");
+    });
+  } else {
+    languageSelectionOtherLangs.forEach(lang => {
+      lang.classList.remove("brown");
+      lang.classList.add("white");
+    });
+  }
+});
