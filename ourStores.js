9700513f85c03c3c4c1c97355661122111dfb6ba
alijalilzadeh <ali.jalilzadeh2007@gmail.com>
2025-12-12 22:24:44 +0400

    Uptaded

--- ourStores.js
+++ ourStores.js
@@ -7,3 +7,26 @@ document.querySelectorAll(".mainLinks").forEach(link => {
     }
   });
 });
+
+// const citiesMapLinks = document.querySelectorAll("districtsOfBaku districtsLinks a");
+// citiesMapLinks.forEach(link => {
+//   const underSection = document.querySelector()
+// });
+
+const citiesArr = [
+  absheron,
+  binagadi,
+  garadagh,
+  khatai,
+  khazar,
+  narimanov,
+  nasimi,
+  nizami,
+  sabail,
+  sabunchi,
+  surakhany,
+  yasamal,
+  ganja,
+  mingachevir,
+  sumgait
+]
