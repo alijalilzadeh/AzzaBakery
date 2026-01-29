be17813a44a7f780d15fd0eba9c6350cc8aa8558
alijalilzadeh <ali.jalilzadeh2007@gmail.com>
2026-01-29 17:27:06 +0400

    Location data js file was created and uptaded

---
+++ locationData.js
@@ -0,0 +1,465 @@
+data = [
+  {
+    "region": "Absheron",
+    "locations": [
+      {
+        "id": 1,
+        "address": "Absheron region, Masazir settlement",
+        "latitude": 40.456694,
+        "longitude": 49.757111
+      },
+      {
+        "id": 2,
+        "address": "House 17, Nabi Khazri Street, Khirdalan City, Absheron District",
+        "latitude": 40.448694,
+        "longitude": 49.750361
+      },
+      {
+        "id": 3,
+        "address": "Absheron district, Jeyranbatan settlement, Baku-Sumgait highway 26th km",
+        "latitude": 40.543305,
+        "longitude": 40.543305
+      },
+      {
+        "id": 4,
+        "address": "Heydar Aliyev Avenue, Novkhani Village, Absheron District",
+        "latitude": 40.537527,
+        "longitude": 49.782667
+      },
+      {
+        "id": 5,
+        "address": "1 Heydar Aliyev Avenue, Apt/Unit 1282, Khirdalan City, Absheron District",
+        "latitude": 40.458806,
+        "longitude": 49.732195
+      },
+      {
+        "id": 6,
+        "address": "Mammadli Village, Absheron District",
+        "latitude": 40.504972,
+        "longitude": 49.945416
+      },
+      {
+        "id": 7,
+        "address": "28 May Street, Mehdiabad Settlement, Absheron District",
+        "latitude": 40.484666,
+        "longitude": 49.854972
+      },
+      {
+        "id": 8,
+        "address": "40 Aliaga Vahid Street, Masazyr Settlement, Absheron District",
+        "latitude": 40.467556,
+        "longitude": 49.754194
+      },
+      {
+        "id": 9,
+        "address": "1 Heydar Aliyev Avenue, Khirdalan City, Absheron District",
+        "latitude": 40.453694,
+        "longitude": 49.740444
+      },
+    ]
+  },
+  {
+    "region": "Binagadi",
+    "locations": [
+      {
+        "id": 10,
+        "address": "236 M. Rasulzade Street, Binagadi District",
+        "latitude": 40.384777,
+        "longitude": 49.853667
+      },
+      {
+        "id": 11,
+        "address": "65B Ataturk Avenue, Binagadi District",
+        "latitude": 40.415528,
+        "longitude": 49.845722
+      },
+      {
+        "id": 12,
+        "address": "Sattar Bahlulzadeh Street, Binagadi District",
+        "latitude": 40.41425,
+        "longitude": 49.840305
+      },
+      {
+        "id": 13,
+        "address": "361B Suleyman Sani Akhundov Street, 7th Microdistrict, Binagadi District",
+        "latitude": 40.428806,
+        "longitude": 49.852333
+      },
+      {
+        "id": 14,
+        "address": "6B M. Davudoghlu Street, Rasulzade Settlement, Binagadi District",
+        "latitude": 40.422889,
+        "longitude": 49.834722
+      },
+      {
+        "id": 15,
+        "address": "394 Ibrahimpasha Dadashov Street, Binagadi District",
+        "latitude": 40.424111,
+        "longitude": 49.842833
+      },
+      {
+        "id": 16,
+        "address": "Suleyman Sani Akhundov Street, 7th Microdistrict, Binagadi District",
+        "latitude": 40.426139,
+        "longitude": 49.847139
+      },
+      {
+        "id": 17,
+        "address": "17 R. Ismailov Street, Bilajari Settlement, Binagadi District",
+        "latitude": 40.420139,
+        "longitude": 49.801667
+      },
+      {
+        "id": 18,
+        "address": "6 Ajami Nakhchvani Street, 8th Microdistrict, Binagadi District",
+        "latitude": 40.415556,
+        "longitude": 49.837472
+      },
+      {
+        "id": 19,
+        "address": "17 Jafar Khandan Street, 8th Microdistrict, Binagadi District",
+        "latitude": 40.418500,
+        "longitude": 49.842917
+      },
+    ]
+  },
+  {
+    "region": "Ganja",
+    "locations": [
+      {
+        "id": 20,
+        "address": "254B entrance, Nariman Narimanov avenue, Ganja city",
+        "latitude": 40.666528,
+        "longitude": 46.361333
+      },
+      {
+        "id": 21,
+        "address": "Ganja city, Dede Korkut street",
+        "latitude": 40.677973,
+        "longitude": 46.373361
+      },
+      {
+        "id": 22,
+        "address": "236 Ataturk avenue, Ganja city",
+        "latitude": 40.677973,
+        "longitude": 46.373361
+      }
+    ]
+  },
+  {
+    "region": "Garadagh",
+    "locations": [
+      {
+        "id": 23,
+        "address": "28 May Street, Lokbatan Settlement, Garadagh District",
+        "latitude": 40.321861,
+        "longitude": 49.729361
+      },
+      {
+        "id": 24,
+        "address": "Emin Guliyev Street, 45, Sahil Settlement, Garadagh District",
+        "latitude": 40.226806,
+        "longitude": 49.583195
+      }
+    ]
+  },
+  {
+    "region": "Khatai",
+    "locations": [
+      {
+        "id": 25,
+        "address": "134A, Neapol street, Khatai district",
+        "latitude": 40.382000,
+        "longitude": 49.963583
+      },
+      {
+        "id": 26,
+        "address": "House 21, Javanshir Street, Khatai District",
+        "latitude": 40.365528,
+        "longitude": 49.948805
+      },
+      {
+        "id": 27,
+        "address": "56 Nasraddin Tusi Street, Khatai District",
+        "latitude": 40.378250,
+        "longitude": 49.959611
+      },
+      {
+        "id": 28,
+        "address": "2930 Ganja Avenue, Khatai District",
+        "latitude": 40.355389,
+        "longitude": 49.961611
+      },
+      {
+        "id": 29,
+        "address": "Intersection of Kh. Mammadov and R. Mammadov, Khatai District",
+        "latitude": 40.372611,
+        "longitude": 49.943389
+      },
+      {
+        "id": 30,
+        "address": "8 Zigh Highway, Khatai District",
+        "latitude": 40.367556,
+        "longitude": 49.940833
+      },
+      {
+        "id": 31,
+        "address": "34C General Shikhlinski Street, Khatai District",
+        "latitude": 40.376473,
+        "longitude": 49.964667
+      },
+      {
+        "id": 32,
+        "address": "45 Khudu Mammadov Street, Khatai District",
+        "latitude": 40.370973,
+        "longitude": 49.957778
+      },
+      {
+        "id": 33,
+        "address": "136 A Mohammad Hadi str., Khatai dist.",
+        "latitude": 40.389361,
+        "longitude": 49.953806
+      },
+      {
+        "id": 34,
+        "address": "62M I. Habibov Street, Babek Avenue, Khatai District",
+        "latitude": 40.392722,
+        "longitude": 49.922472
+      },
+      {
+        "id": 35,
+        "address": "22 S. Vazirov Street, Khatai District",
+        "latitude": 40.383944,
+        "longitude": 49.872195
+      },
+      {
+        "id": 36,
+        "address": "29C M. Rustamov Street, Khatai District",
+        "latitude": 40.385750,
+        "longitude": 49.961944
+      },
+      {
+        "id": 37,
+        "address": "2945 M. Hady Street, Khatai District",
+        "latitude": 40.374306,
+        "longitude": 49.953083
+      },
+      {
+        "id": 38,
+        "address": "8 November Avenue, Khatai District",
+        "latitude": 40.377195,
+        "longitude": 49.869056
+      },
+      {
+        "id": 39,
+        "address": "10 Y. Safarov Street, Khatai District",
+        "latitude": 40.378195,
+        "longitude": 49.865389
+      }
+    ]
+  },
+  {
+    "region": "Khazar",
+    "locations": [
+      {
+        "id": 40,
+        "address": "27 Ali Isazadeh Street, Bina Settlement, Khazar District",
+        "latitude": 40.423695,
+        "longitude": 50.102750
+      },
+      {
+        "id": 41,
+        "address": "Istiklal Street, Buzovna Settlement, Khazar District",
+        "latitude": 40.516528,
+        "longitude": 50.101583
+      },
+      {
+        "id": 42,
+        "address": "Buzovna Shuvelan road",
+        "latitude": 40.511194,
+        "longitude": 50.140833
+      },
+      {
+        "id": 43,
+        "address": "Mardakan Highway, Gala Settlement, Khazar District",
+        "latitude": 40.470973,
+        "longitude": 50.103278
+      },
+      {
+        "id": 44,
+        "address": "16 Pobeda Street, Khazar District",
+        "latitude": 40.525556,
+        "longitude": 50.098694
+      },
+      {
+        "id": 45,
+        "address": "2C Ali Isazadeh Street, Bina Settlement, Khazar District",
+        "latitude": 40.447083,
+        "longitude": 50.089444
+      },
+      {
+        "id": 46,
+        "address": "92C Yesenin Street, Mardakan Settlement, Khazar District",
+        "latitude": 40.487528,
+        "longitude": 50.158917
+      },
+      {
+        "id": 47,
+        "address": "35 Almas Ildirym Street, Shuvalan Settlement, Khazar District",
+        "latitude": 40.485389,
+        "longitude": 50.175611
+      },
+      {
+        "id": 48,
+        "address": "92 Yesenin Street, Mardakan Settlement, Khazar District",
+        "latitude": 40.488528,
+        "longitude": 50.139833
+      },
+    ]
+  },
+  {
+    "region": "Mingachevir",
+    "locations": [
+      {
+        "id": 49,
+        "address": "Mingachevir, M. A. Rasulzadeh",
+        "latitude": 40.771611,
+        "longitude": 47.040556
+      }
+    ]
+  },
+  {
+    "region": "Narimanov",
+    "locations": [
+      {
+        "id": 50,
+        "address": "Ulvi Bunyadzade 2, Narimanov district",
+        "latitude": 40.385222,
+        "longitude": 49.851028
+      },
+      {
+        "id": 51,
+        "address": "57x Ibrahimpasha Dadashov street, Narimanov district",
+        "latitude": 40.410806,
+        "longitude": 49.844056
+      },
+      {
+        "id": 52,
+        "address": "5 Khatai Avenue, Narimanov District",
+        "latitude": 40.384972,
+        "longitude": 49.853583
+      },
+      {
+        "id": 53,
+        "address": "26 Aliyar Aliyev Street, Nariman Narimanov",
+        "latitude": 40.406667,
+        "longitude": 49.875639
+      },
+      {
+        "id": 54,
+        "address": "40B Karabakh Street, Narimanov District",
+        "latitude": 40.388611,
+        "longitude": 49.856694
+      },
+      {
+        "id": 55,
+        "address": "27L Heydar Aliyev Avenue, Narimanov District",
+        "latitude": 40.399278,
+        "longitude": 49.875222
+      },
+      {
+        "id": 56,
+        "address": "1979 District, Najaf Narimanov Street, Narimanov District",
+        "latitude": 40.408278,
+        "longitude": 49.837194
+      },
+      {
+        "id": 57,
+        "address": "18 Ashiq Molla Juma Street, Narimanov District",
+        "latitude": 40.399972,
+        "longitude": 49.869333
+      },
+      {
+        "id": 58,
+        "address": "161C Aliyar Aliyev Street, Narimanov District",
+        "latitude": 40.368806,
+        "longitude": 49.836667
+      },
+      {
+        "id": 59,
+        "address": "Fatali Khan Khoyski Avenue, Narimanov District",
+        "latitude": 40.402806,
+        "longitude": 49.863306
+      },
+      {
+        "id": 60,
+        "address": "22/2A Khoroqlu Rahimov Street, Narimanov District",
+        "latitude": 40.400250,
+        "longitude": 49.858722
+      },
+      {
+        "id": 61,
+        "address": "38 Khatai Avenue, Narimanov District",
+        "latitude": 40.391472,
+        "longitude": 49.862472
+      },
+      {
+        "id": 62,
+        "address": "22 J. Hacibeyli Street, Narimanov District",
+        "latitude": 40.387444,
+        "longitude": 49.848056
+      },
+      {
+        "id": 63,
+        "address": "Fatali Khan Khoyski Avenue, Narimanov District",
+        "latitude": 40.402806,
+        "longitude": 49.863306
+      },
+      {
+        "id": 64,
+        "address": "22/2A Khoroqlu Rahimov Street, Narimanov District",
+        "latitude": 40.400250,
+        "longitude": 49.858722
+      },
+      {
+        "id": 65,
+        "address": "Fatali Khan Khoyski Avenue, Narimanov District",
+        "latitude": 40.402806,
+        "longitude": 49.863306
+      },
+      {
+        "id": 66,
+        "address": "22/2A Khoroqlu Rahimov Street, Narimanov District",
+        "latitude": 40.400250,
+        "longitude": 49.858722
+      },
+    ]
+  }
+]
+
+const mapFrame = document.querySelector(".map-wrapper iframe");
+const locationBoxes = document.querySelectorAll(".locationLinks .underLinks");
+
+locationBoxes.forEach(box => {
+  box.addEventListener("click", () => {
+    const locationName = box.querySelector("p").innerText.trim();
+
+    let foundLat = null;
+    let foundLong = null;
+
+    for (const region of data) {
+      const foundLocation = region.locations.find(loc => loc.address === locationName);
+
+      if (foundLocation) {
+        foundLat = foundLocation.latitude;
+        foundLong = foundLocation.longitude;
+      }
+    }
+    if (foundLat && foundLong) {
+      mapFrame.src = `https://www.google.com/maps?q=${foundLat},${foundLong}&z=16&output=embed`
+    }
+    else {
+      console.log("Location wasn't found")
+    }
+  });
+});
