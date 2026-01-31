3a36a04c190f72d6f25c98dba736842fa2a5bea0
alijalilzadeh <ali.jalilzadeh2007@gmail.com>
2026-01-31 21:53:01 +0400

    More details page upgraded

--- moreDetailsData.js
+++ moreDetailsData.js
@@ -27,15 +27,42 @@ const moreDetailData = [
         "size": "10 (person) - 19.90 AZN",
         "images": ["mascotte1.jpg", "mascotte2.jpg", "mascotte3.jpg"]
       },
-        {
+      {
         "id": 4,
         "name": "Mocha",
         "price": 12.90,
         "ingredients": "dark biscuit crumbs, fine flour, powdered sugar, baking powder, eggs, emulsifier, cocoa, drinking water, caramel jelly, vanilla powder, corn starch, butter, milk, condensed milk, walnut kernels, hazelnut kernels, powdered sugar",
-        "size": ["5 (person) - 12.90 AZN","10 (person) - 22.90 AZN","15 (person) - 29.90 AZN" ],
+        "size": ["5 (person) - 12.90 AZN", "10 (person) - 22.90 AZN", "15 (person) - 29.90 AZN"],
+        "images": ["mocha1.jpg", "mocha2.jpg", "mocha3.jpg"]
+      },
+      {
+        "id": 5,
+        "name": "Skazka Chocolate",
+        "price": 13.90,
+        "ingredients": "vanilla, water, syrup, gelatin, cocoa, marmalade, butter, condensed milk, flour, powdered sugar, baking powder, vegetable cream, black chocolate, emulsifier, eggs.",
+        "size": "5 (person) - 13.90 AZN",
         "images": ["mocha1.jpg", "mocha2.jpg", "mocha3.jpg"]
       },
     ]
 
   }
 ]
+
+const pageSwiper = new Swiper(".paginationContent", {
+  loop: true,
+  speed: 800,
+  centeredSlides: true,
+  spaceBetween: 5,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  }
+})
+
+const moreDetailsLinks = document.querySelectorAll(".productBox a");
