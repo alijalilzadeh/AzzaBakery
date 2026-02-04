704a5feca656c603e8975cbcd0ac8dbfb5bbbde9
alijalilzadeh <ali.jalilzadeh2007@gmail.com>
2026-02-04 19:45:16 +0400

    I created getYourLocation.js file it works when you click crosshairs-location icon, it returns you your location in google map

---
+++ getYourLocation.js
@@ -0,0 +1,17 @@
+const locationIcon = document.querySelector(".locationSelect .fa-location-crosshairs");
+const mapFrame = document.querySelector(".map-wrapper iframe")
+locationIcon.addEventListener("click",()=>{
+  if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(onSucces,onError);
+  }
+  async function onSucces(position){
+    lat = position.coords.latitude;
+    long = position.coords.longitude;
+    mapFrame.src = `https://www.google.com/maps?q=${lat},${long}&z=16&output=embed`
+
+  }
+
+  async function onError(err){
+    console.log(err)
+  }
+})
