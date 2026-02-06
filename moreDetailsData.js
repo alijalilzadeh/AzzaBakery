0d5aed5b1df91f137f689bf4ad4b59f6386b3c36
alijalilzadeh <ali.jalilzadeh2007@gmail.com>
2026-02-07 00:48:22 +0400

    I added new Products details information content to webpage, you can easily see any information about product such as : price, size, ingredients

--- moreDetailsData.js
+++ moreDetailsData.js
@@ -1,13 +1,13 @@
 const moreDetailData = [
   {
     "product": "cake",
-    "Product Names": [
+    "ProductInfo": [
       {
         "id": 1,
         "name": "Noisette",
         "price": 21.90,
         "ingredients": "White sponge cake crumbs, vanilla powder, drinking water, gelatin, butter, condensed milk, finely milled flour, almonds, hazelnuts, powdered sugar, baking powder, vegetable cream, milk toffee paste, salt, emulsifier, eggs.",
-        "size": "10 (person) - 21.90 AZN",
+        "size": ["10 (person) - 21.90 AZN"],
         "images": ["noisette.jpg", "noisette2.jpg", "noisette3.jpg"]
 
       },
@@ -16,7 +16,7 @@ const moreDetailData = [
         "name": "Raspberry yogurt",
         "price": 19.90,
         "ingredients": "White biscuit crumbs, fine flour, powdered sugar, baking powder, vanilla powder, eggs, emulsifier, drinking water, milk, plant-based cream, gelatin, yogurt, frozen raspberries, cornstarch, butter, powdered sugar",
-        "size": "10 (person) - 19.90 AZN",
+        "size": ["10 (person) - 19.90 AZN"],
         "images": ["raspberryYogurt1.jpg", "raspberryYogurt2.jpg", "raspberryYogurt3.jpg"]
       },
       {
@@ -24,7 +24,7 @@ const moreDetailData = [
         "name": "Mascotte",
         "price": 19.90,
         "ingredients": "dark biscuit crumbs, fine flour, powdered sugar, baking powder, eggs, emulsifier, cocoa, drinking water, caramel jelly, vanilla powder, corn starch, butter, milk, condensed milk, walnut kernels, hazelnut kernels, powdered sugar",
-        "size": "10 (person) - 19.90 AZN",
+        "size": ["10 (person) - 19.90 AZN"],
         "images": ["mascotte1.jpg", "mascotte2.jpg", "mascotte3.jpg"]
       },
       {
@@ -39,30 +39,218 @@ const moreDetailData = [
         "id": 5,
         "name": "Skazka Chocolate",
         "price": 13.90,
-        "ingredients": "vanilla, water, syrup, gelatin, cocoa, marmalade, butter, condensed milk, flour, powdered sugar, baking powder, vegetable cream, black chocolate, emulsifier, eggs.",
-        "size": "5 (person) - 13.90 AZN",
-        "images": ["mocha1.jpg", "mocha2.jpg", "mocha3.jpg"]
+        "ingredients": "vanilla, water, syrup, gelatin, cocoa, marmalade, butter, condensed milk, flour, powdered sugar, baking powder, vegetable cream, black chocolate, emulsifier, eggs",
+        "size": ["5 (person) - 13.90 AZN"],
+        "images": ["skazkachocolate1.jpg", "skazkachocolate2.jpg", "skazkachocolate3.jpg"]
+      },
+      {
+        "id": 6,
+        "name": "Cherry",
+        "price": 9.90,
+        "ingredients": "flour, butter, eggs, powdered sugar, condensed milk, canned cherries, walnuts, almonds, yogurt, gelatin, white jelly, plain jelly, cocoa, vegetable-based cream, vanilla powder, emulsifier, water",
+        "size": ["5 (person) - 9.90 AZN"],
+        "images": ["cherry1.png", "cherry2.png"]
+      },
+      {
+        "id": 7,
+        "name": "Skazka",
+        "price": 13.90,
+        "ingredients": "vanilla, water, cocoa, marmalade, butter, condensed milk, flour, sugar, baking powder, vegetable cream, emulsifier, eggs.",
+        "size": ["5 (person) - 13.50 AZN"],
+        "images": ["skazka1.jpg", "skazka2.jpg"]
+      },
+      {
+        "id": 8,
+        "name": "Trufel",
+        "price": 12.90,
+        "ingredients": "vanilla, water, cocoa, butter, condensed milk, flour, sugar, baking powder, vegetable cream, black chocolate, emulsifier, eggs.",
+        "size": ["5 (person) - 12.50 AZN", "10 (person) - 22.50 AZN", "15 (person) - 29.90 AZN", "20 (person) - 34.90 AZN"],
+        "images": ["trufel1.jpg", "trufel2.jpg"]
+      },
+      {
+        "id": 9,
+        "name": "Kudrash",
+        "price": 17.90,
+        "ingredients": "vanilla, water, cocoa, butter, condensed milk, flour, sugar, baking powder, vegetable cream, black chocolate, emulsifier, eggs.",
+        "size": ["10 (person) - 17.90 AZN"],
+        "images": ["kudrash1.jpg", "kudrash2.jpg", "kudrash3.jpg"]
+      },
+      {
+        "id": 10,
+        "name": "Mix fruit",
+        "price": 19.90,
+        "ingredients": "flour, eggs, sugar, black chocolate, vegetable cream, seasonal fruit, mango flavor, cherry jelly, baking powder, vanilla, gelatin, jelly, emulsifier, water. Frozen: cherry, pineapple, mandarin, peach.",
+        "size": ["10 (person) - 19.90 AZN"],
+        "images": ["mixfruit.jpg"]
+      },
+      {
+        "id": 11,
+        "name": "Mars",
+        "price": 12.90,
+        "ingredients": "flour, butter, eggs, sugar, milk chocolate, black chocolate, boiled condensed milk, vegetable cream, baking powder, vanilla, emulsifier, water.",
+        "size": ["5 (person) - 12.90 AZN", "10 (person) - 22.90 AZN", "15 (person) - 29.90 AZN", "20 (person) - 36.90 AZN"],
+        "images": ["mars1.jpg", "mars2.jpg"]
+      },
+      {
+        "id": 12,
+        "name": "Black Forest",
+        "price": 19.90,
+        "ingredients": "egg, flour, sugar, milk, jelly, starch, cocoa, vegetable cream, milk chocolate, dark chocolate, emulsifier, canned cherries, water.",
+        "size": ["10 (person) - 19.90 AZN"],
+        "images": ["blackforest1.jpg", "blackforest2.jpg", "blackforest3.jpg"]
+      },
+      {
+        "id": 13,
+        "name": "Summer Pancake",
+        "price": 24.90,
+        "ingredients": "flour, sunflower oils, egg, sugar, powdered sugar, milk, mascarpone cheese, strawberry, seasonal fruits, cream, vanilla powder, caramel jelly, salt, canned peaches and canned pineapple.",
+        "size": ["10 (person) - 24.90 AZN"],
+        "images": ["summerpancake1.png", "summerpancake2.png", "summerpancake3.png"]
+      },
+      {
+        "id": 14,
+        "name": "Napoleon",
+        "price": 12.90,
+        "ingredients": "flour,butter, eggs, sugar,milk, vanilla, salt, water.",
+        "size": ["5 (person) - 12.90 AZN"],
+        "images": ["napoleon.jpg", "napoleon2.jpg"]
+      },
+      {
+        "id": 15,
+        "name": "Mix berry",
+        "price": 19.90,
+        "ingredients": "flour, butter, eggs, sugar, almonds, labne cheese, cottage cheese, strawberry flavor, vegetable cream, food color, gelatin, jelly,starch, vanilla, water, frozen: red currants, black currants.",
+        "size": ["10 (person) - 19.90 AZN"],
+        "images": ["mixberry1.jpg", "mixberry2.jpg"]
+      },
+      {
+        "id": 16,
+        "name": "Tiramisu",
+        "price": 11.90,
+        "ingredients": "flour, eggs , sugar, cocoa, coffee, dark chocolate, mascarpone cheese, vegetable cream, gelatin, water.",
+        "size": ["5 (person) - 11.90 AZN"],
+        "images": ["tiramisu1.png", "tiramisu2.png", "tiramisu3.png"]
+      },
+      {
+        "id": 17,
+        "name": "Profiterole",
+        "price": 26.90,
+        "ingredients": "vanilla, water, white jelly, caramel jelly, cocoa, butter, milk, condensed milk, flour, walnuts, almonds, prolayn, sugar, baking powder, vegetable cream, salt, emulsifier, eggs.",
+        "size": ["10 (person) - 26.90 AZN", "15 (person) - 32.90 AZN"],
+        "images": ["profiterole1.jpg", "profiterole2.jpg", "profiterole3.jpg"]
+      },
+      {
+        "id": 18,
+        "name": "Absheron",
+        "price": 22.90,
+        "ingredients": "vanilla, water, chocolate jelly, cocoa, butter, boiled condensed milk, flour, sugar, baking powder, emulsifier, eggs.",
+        "size": ["10 person -22.90 AZN"],
+        "images": ["absheron1.jpg", "absheron2.jpg"]
       },
     ]
 
   }
 ]
 
-const pageSwiper = new Swiper(".paginationContent", {
-  loop: true,
-  speed: 800,
-  centeredSlides: true,
-  spaceBetween: 5,
+console.log(moreDetailData[0].ProductInfo)
 
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+const moreDetailBoxes = document.querySelectorAll(".productBox a");
+const existingContent = document.querySelector(".mainContentPart");
+const bannerText = document.querySelector(".bannerText");
+moreDetailBoxes.forEach(link => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
 
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  }
+    const productBox = link.closest(".productBox");
+    const productName = productBox.querySelector(".cakename").innerText;
+    const productType = productBox.querySelector(".cakeType")
+
+    bannerText.style.display = "none";
+
+    const productData = moreDetailData[0]["ProductInfo"].find(cake => cake.name === productName);
+    if (!productData) return;
+    if (existingContent) existingContent.remove();
+
+    const currenPageLocation = window.location.pathname.split("/").pop();
+    console.log(currenPageLocation)
+    if (!document.querySelector(".detailContent")) {
+      const newLayoutContent = document.createElement("div");
+
+      newLayoutContent.className = "detaiContent";
+      newLayoutContent.innerHTML =
+        `
+      <div class="reverseIcon">
+              <a href="${currenPageLocation}">
+                <i class="fa-solid fa-arrow-left"></i>
+              </a>
+      </div>
+
+     <div class="detailBox">
+        <div class="swiper paginationContent">
+          <div class="swiper-wrapper">
+            ${productData.images.map(img =>
+          `
+                <div class="swiper-slide">
+                  <img src="MoreDetailedProducts/paginationCake/${img}" alt="${img} Cake">
+                </div>
+              `
+        ).join('')}
+          </div>
+          <div class="swiper-button-prev">
+            <i class="fa-solid fa-arrow-left"></i>
+          </div>
+
+          <div class="swiper-button-next">
+            <i class="fa-solid fa-arrow-right"></i>
+          </div>
+
+          <div class="swiper-pagination"></div>
+        </div>
+
+        <div class="productDetailInfo">
+            
+          <div>
+            <h2 class="product-name">${productName}</h2>
+            <p class="product-price">
+              <span>PRICE</span>
+              <span class="price-value">${productData.price}0 AZN</span>
+            </p>
+            <div class="product-ingredients">
+              <h3>INGREDIENTS</h3>
+              <p>${productData.ingredients}</p>
+            </div>
+            <div class="product-size">
+              <span>SIZE</span>
+              
+              ${productData.size.map(s =>
+          `
+                <span class="size-value">
+                 ${s}
+                </span>
+                `
+        ).join('')}
+              
+            </div>
+            <button class="shareBtn">SHARE FACEBOOK</button>
+          </div>
+        </div>
+      </div>
+    `
+      bannerText.insertAdjacentElement("beforebegin", newLayoutContent)
+
+      new Swiper(".paginationContent", {
+        loop: true,
+        speed: 800,
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        }
+      });
+    }
+  })
 })
 
-const moreDetailsLinks = document.querySelectorAll(".productBox a");
